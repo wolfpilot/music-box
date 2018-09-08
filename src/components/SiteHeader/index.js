f2583e8a0732f13ba977cd393f83000548951842
Razvan Negrea <howl@wolfpilot.co.uk>
2018-09-08 20:26:48 +0200

    feat(notification): Add user notification button

--- src/components/SiteHeader/index.js
+++ src/components/SiteHeader/index.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 
 // Components
 import Logo from '../Logo';
+import Notification from '../Notification';
 
 class SiteHeader extends Component {
   render() {
@@ -10,6 +11,10 @@ class SiteHeader extends Component {
       <header className="site-header">
         <div className="site-header__logo">
           <Logo />
+        </div>
+
+        <div className="site-header__actions">
+          <Notification />
         </div>
       </header>
     );
