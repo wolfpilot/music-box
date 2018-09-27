5badb26889f48ea5eecffe5fc29a6cd8b9a5044b
Razvan Negrea <howl@wolfpilot.co.uk>
2018-09-27 22:01:20 +0200

    feat(debugging): Add dependency @redux-logger for debugging state in console

--- src/store.js
+++ src/store.js
@@ -1,9 +1,13 @@
 // Libs
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 
 // Utils
 import rootReducer from './reducers';
 
-const configureStore = initialState => createStore(rootReducer, initialState);
+const logger = createLogger();
+const createStoreWithMiddleware = applyMiddleware(logger)(createStore);
+const configureStore = initialState =>
+  createStoreWithMiddleware(rootReducer, initialState);
 
 export default configureStore;
