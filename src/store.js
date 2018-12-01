b30dd36a4e942e1f67609f586016b445819a1448
Razvan Negrea <howl@wolfpilot.co.uk>
2018-12-01 18:32:39 +0100

    chore(store): Set persistor state reconciler to shallow auto merge Lvl2

--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@
 import { applyMiddleware, compose, createStore } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import { connectRouter, routerMiddleware } from 'connected-react-router';
 import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
@@ -18,6 +19,7 @@ const router = routerMiddleware(history);
 const persistConfig = {
   key: 'root',
   storage,
+  stateReconciler: autoMergeLevel2,
   whitelist: ['session', 'user']
 };
 
