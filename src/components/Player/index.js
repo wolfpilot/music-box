46393fbce2583f1c6616ffbe1ca1fe9e360e4d5f
Razvan Negrea <howl@wolfpilot.co.uk>
2019-05-03 22:49:29 +0200

    feat(player): Update track progress while played

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -5,6 +5,11 @@ import PropTypes from 'prop-types';
 
 // Utils
 import { playTrack, pauseTrack } from '../../actions';
+import { formatMilliseconds } from '../../static/js/utils/mathHelpers';
+
+const defaults = {
+  previewDuration: 30
+};
 
 const mapStateToProps = state => {
   return {
@@ -18,14 +23,153 @@ const mapDispatchToProps = dispatch => ({
 });
 
 class Player extends Component {
-  componentDidMount() {
+  constructor() {
+    super();
+
+    this._onTimeUpdate = this._onTimeUpdate.bind(this);
+  }
+
+  state = {
+    currentTime: 0
+  };
+
+  getArtists(track) {
+    return track.artists.map(artist => artist.name).join(', ');
+  }
+
+  _updateProgress() {
+    this.setState({ currentTime: this.audio.currentTime });
+  }
+
+  _onTimeUpdate() {
+    // Normally, the current play time should be compared to the total track duration
+    // however that's not possible due to Spotify only providing 30s previews.
+    if (this.audio.currentTime >= defaults.previewDuration) {
+      // @TODO: playNextTrack()
+
+      return;
+    }
+
+    this._updateProgress();
+  }
+
+  _renderPlaybackOptions() {
+    return (
+      <div className="player__controls">
+        <div className="player__volume">
+          <button
+            type="button"
+            className="player__btn player__volume-toggle-btn"
+          >
+            V<span className="sr-only">Volume</span>
+          </button>
+
+          <div className="player__volume-controls">
+            <div className="player__volume-track" role="presentation" />
+            <div className="player__volume-bar" role="presentation" />
+            <div className="player__volume-thumb" role="presentation" />
+          </div>
+        </div>
+        <button type="button" className="player__btn player__btn-shuffle">
+          S<span className="sr-only">Shuffle</span>
+        </button>
+        <button type="button" className="player__btn player__btn-repeat">
+          R<span className="sr-only">Repeat</span>
+        </button>
+      </div>
+    );
+  }
+
+  _renderTimeline() {
+    const { track } = this.props.player;
+
+    return (
+      <div className="player__timeline">
+        <div className="player__elapsed-time">
+          {formatMilliseconds(this.state.currentTime * 1000)}
+        </div>
+
+        <div className="player__progress">
+          <div className="player__progress-track" role="presentation" />
+          <div className="player__progress-bar" role="presentation" />
+          <div className="player__progress-thumb" role="presentation" />
+        </div>
+
+        <div className="player__total-time">
+          {formatMilliseconds(track.duration_ms)}
+        </div>
+      </div>
+    );
+  }
+
+  _renderPlaybackControls() {
+    const { isPlaying, track } = this.props.player;
+
+    return (
+      <div className="player__controls">
+        <button type="button" className="player__btn player__btn-prev">
+          &#124;&#60;<span className="sr-only">Go to previous track</span>
+        </button>
+        {isPlaying ? (
+          <button
+            type="button"
+            className="player__btn player__btn-pause"
+            onClick={this.props.pauseTrack}
+          >
+            &#9647;&#9647;<span className="sr-only">Pause</span>
+          </button>
+        ) : (
+          <button
+            type="button"
+            className="player__btn player__btn-play"
+            onClick={() => this.props.playTrack(track)}
+          >
+            &#9655;<span className="sr-only">Play</span>
+          </button>
+        )}
+        <button type="button" className="player__btn player__btn-next">
+          &#62;&#124;<span className="sr-only">Go to next track</span>
+        </button>
+      </div>
+    );
+  }
+
+  _renderTrackDetails() {
+    const { track } = this.props.player;
+
+    return (
+      <div className="player__active-track">
+        <div className="player__artwork-wrapper">
+          {track.artwork && (
+            <img
+              className="player__artwork-image"
+              src={track.artwork.url}
+              alt=""
+            />
+          )}
+        </div>
+
+        <div className="player__track-meta">
+          <div className="player__track-name">
+            {track.name ? track.name : ''}
+          </div>
+          <div className="player__artist-name">{this.getArtists(track)}</div>
+        </div>
+      </div>
+    );
+  }
+
+  _setupAudio() {
     this.audio = new Audio();
 
     this.audio.loop = false;
+
+    this.audio.addEventListener('timeupdate', this._onTimeUpdate);
   }
 
-  // @TODO: onPlay, onPause? Prevent play if already playing, same for pause.
-  // @TODO: Otherwise, creates too many unnecessary Redux store updates
+  componentDidMount() {
+    this._setupAudio();
+  }
 
   componentDidUpdate(prevProps) {
     const { isPlaying, track } = this.props.player;
@@ -38,96 +182,21 @@ class Player extends Component {
     isPlaying ? this.audio.play() : this.audio.pause();
   }
 
-  getArtists(track) {
-    return track.artists.map(artist => artist.name).join(', ');
+  componentWillUnmount() {
+    this.audio.removeEventListener('timeupdate', this._onTimeUpdate);
   }
 
   render() {
-    const { isPlaying, track } = this.props.player;
+    const { track } = this.props.player;
 
     return (
       <div className="player">
         {track && (
           <>
-            <div className="player__active-track">
-              <div className="player__artwork-wrapper">
-                <img
-                  className="player__artwork-image"
-                  src={track.artwork.url}
-                  alt=""
-                />
-              </div>
-
-              <div className="player__track-meta">
-                <div className="player__track-name">
-                  {track.name ? track.name : ''}
-                </div>
-                <div className="player__artist-name">
-                  {this.getArtists(track)}
-                </div>
-              </div>
-            </div>
-
-            <div className="player__controls">
-              <button type="button" className="player__btn player__btn-prev">
-                &#124;&#60;<span className="sr-only">Go to previous track</span>
-              </button>
-              {isPlaying ? (
-                <button
-                  type="button"
-                  className="player__btn player__btn-pause"
-                  onClick={this.props.pauseTrack}
-                >
-                  &#9647;&#9647;<span className="sr-only">Pause</span>
-                </button>
-              ) : (
-                <button
-                  type="button"
-                  className="player__btn player__btn-play"
-                  onClick={() => this.props.playTrack(track)}
-                >
-                  &#9655;<span className="sr-only">Play</span>
-                </button>
-              )}
-              <button type="button" className="player__btn player__btn-next">
-                &#62;&#124;<span className="sr-only">Go to next track</span>
-              </button>
-            </div>
-
-            <div className="player__timeline">
-              <div className="player__elapsed-time">02:15</div>
-
-              <div className="player__progress">
-                <div className="player__progress-track" role="presentation" />
-                <div className="player__progress-bar" role="presentation" />
-                <div className="player__progress-thumb" role="presentation" />
-              </div>
-
-              <div className="player__total-time">04:45</div>
-            </div>
-
-            <div className="player__controls">
-              <div className="player__volume">
-                <button
-                  type="button"
-                  className="player__btn player__volume-toggle-btn"
-                >
-                  V<span className="sr-only">Volume</span>
-                </button>
-
-                <div className="player__volume-controls">
-                  <div className="player__volume-track" role="presentation" />
-                  <div className="player__volume-bar" role="presentation" />
-                  <div className="player__volume-thumb" role="presentation" />
-                </div>
-              </div>
-              <button type="button" className="player__btn player__btn-shuffle">
-                S<span className="sr-only">Shuffle</span>
-              </button>
-              <button type="button" className="player__btn player__btn-repeat">
-                R<span className="sr-only">Repeat</span>
-              </button>
-            </div>
+            {this._renderTrackDetails()}
+            {this._renderPlaybackControls()}
+            {this._renderTimeline()}
+            {this._renderPlaybackOptions()}
           </>
         )}
       </div>
